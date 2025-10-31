3ad981b09db187aa04cf5fd5d53e3c644d8476e5
nolang22 <rnrmfwwkd22@gmail.com>
2025-10-31 17:10:42 +0900

    오전 수업 끝 2025 10 31

--- f-iot-react/src/pages/a_basic/index.tsx
+++ f-iot-react/src/pages/a_basic/index.tsx
@@ -10,6 +10,8 @@ import G_props from './G_props'
 import H_Props from './H_Props'
 import I_Rendering from './I_Rendering'
 import J_Handler from './J_Handler'
+import Z_Example01 from './Z_Example01'
+import Z_Example02 from './Z_Example02'
 
 const h2Style= {
   backgroundColor: 'black',
@@ -53,6 +55,12 @@ function Index() {
 
         <h2 style={h2Style}>6. Handler: 리액트의 이벤트 핸들러</h2>
         <J_Handler />
+
+
+        <h2 style={h2Style}>z. 뿌렉티스</h2>
+        <Z_Example01 />
+        <Z_Example02 />
+        
     </div>
   )
 }
---
+++ f-iot-react/src/pages/b_Route/A_DashBoardStats.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function A_DashBoardStats() {
+  return (
+    <div>A_DashBoardStats</div>
+  )
+}
+
+export default A_DashBoardStats
